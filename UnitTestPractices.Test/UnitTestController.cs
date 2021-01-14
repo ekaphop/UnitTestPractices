d1a4e61cbb158aa27d0d08cb27c1422cbe99850d
tiggerz <55571083+tiggerz@users.noreply.github.com>
2021-01-14 20:47:43 +0700

    UnitTest Practices

---
+++ UnitTestPractices.Test/UnitTestController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTestPractices.BSL;
+using UnitTestPractices.Controllers;
+using UnitTestPractices.Data;
+using Xunit;
+
+namespace UnitTestPractices.Test
+{
+    public class UnitTestController
+    {
+        private Mock<ILogger<CustomerProfileController>> logger = new Mock<ILogger<CustomerProfileController>>();
+        private ICustomerService customer = new CustomerService(new CustomerRepository());
+
+        [Fact(DisplayName ="Get Customers, Success")]
+        public void GetCustomersIsSuccess()
+        {
+            var controller = new CustomerProfileController(logger.Object, customer);
+            var result = controller.Customers();
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Theory(DisplayName = "Get Customer by id, OkObjectResult")]
+        [InlineData(1)]
+        public void GetCustomerByIdSuccess(int cusId)
+        {
+            var controller = new CustomerProfileController(logger.Object, customer);
+            var result = controller.Customer(cusId);
+            //Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Theory(DisplayName = "Get Customer by id, BadRequestResult")]
+        [InlineData(0)]
+        public void GetCustomerByIdBadRequest(int cusId)
+        {
+            var controller = new CustomerProfileController(logger.Object, customer);
+            var result = controller.Customer(cusId);
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Theory(DisplayName = "Get Customer by id, NoContentResult")]
+        [InlineData(7)]
+        public void GetCustomerByIdNoContentResult(int cusId)
+        {
+            var controller = new CustomerProfileController(logger.Object, customer);
+            var result = controller.Customer(cusId);
+            Assert.IsType<NoContentResult>(result);
+        }
+    }
+}
